ccd068d6ca58e064fa1b6b06f4cd08042f0e6dae
Oscar Lara <me@oscarlara.dev>
2022-12-06 00:24:38 -0600

    day 5, part 2

--- day5/index.js
+++ day5/index.js
@@ -23,18 +23,33 @@ for (let i = 0; i < columns.length; i++) {
         columnSeparatedCrates[columns[i].toString()].push(columnSeparatedRows[j][i])
     }
 }
+const firstResultCrates = {...columnSeparatedCrates}
+const secondResultCrates = JSON.parse(JSON.stringify(columnSeparatedCrates))
 
 const splittedInstructions = instructions.split('\n')
     .map(line => line.split(' ').filter(word => !isNaN(word)))
 // Moves the crates according to the instructions
 splittedInstructions.forEach(([move, from, to]) => {
-    const cratesToMove = columnSeparatedCrates[from].splice(0, parseInt(move))
-    cratesToMove.forEach(crate => columnSeparatedCrates[to].unshift(crate))
+    const cratesToMove = firstResultCrates[from].splice(0, parseInt(move))
+    cratesToMove.forEach(crate => firstResultCrates[to].unshift(crate))
 })
 
 const squareBracketsRegExp = /\[|\]/g
-const cratesOnTop = Object.values(columnSeparatedCrates)
+const cratesOnTop = Object.values(firstResultCrates)
     .map(column => column[0].replaceAll(squareBracketsRegExp, ''))
     .join('')
 
 console.log(`The crates on the top are: ${cratesOnTop}`);
+
+console.log('- - - - - Part two of the challenge - - - - -');
+
+splittedInstructions.forEach(([move, from, to]) => {
+    const cratesToMove = secondResultCrates[from].splice(0, parseInt(move))
+    secondResultCrates[to].unshift(...cratesToMove)
+})
+
+const cratesOnTopSecondPart = Object.values(secondResultCrates)
+    .map(column => column[0].replaceAll(squareBracketsRegExp, ''))
+    .join('')
+
+console.log(`The crates on the top (for the second part) are: ${cratesOnTopSecondPart}`);
